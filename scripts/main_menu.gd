43a710173cc02d230c5a6720901c1d998aa9ca81
Jack Sims <123803257+sym-z@users.noreply.github.com>
2025-01-10 13:41:46 -0800

    changed naming convention of function. merging

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,10 +2,10 @@ extends Node2D
 
 func _ready() -> void:
 	# Prevent process interruption during scene transition.
-	call_deferred("startGame")
+	call_deferred("start_game")
 
 ## Starts the player in the first level, Limbo.
-func startGame():
+func start_game():
 	get_tree().change_scene_to_file("res://scenes/limbo.tscn")
 
 func _process(delta: float) -> void:
