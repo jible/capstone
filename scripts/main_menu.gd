72257d5f808f92a230a2d8ee6bf06a083b2c87f4
Jack Sims <123803257+sym-z@users.noreply.github.com>
2025-01-10 14:57:24 -0800

    added play button to transition from main menu scene to limbo scene

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,8 +1,8 @@
-extends Node2D
+extends Control
 
 ## The scene to transition to when the player hits "Start"
 @export var scene_to_change : PackedScene
 
-func _ready() -> void:
+func _on_play_button_button_up() -> void:
 	## Switch to the next scene without interuppting a process
 	Globals.change_scene(scene_to_change)
