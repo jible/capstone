d93725cda00b702193a961ddf3a7a2df18720aad
jible <jamezman88th@gmail.com>
2025-01-25 18:40:03 -0800

    cleaned up camera and player

--- scenes/prefabs/player/player.gd
+++ scenes/prefabs/player/player.gd
@@ -2,8 +2,6 @@ extends CharacterBody2D
 
 
 @onready var mobility_manager= $"Mobility Manager"
-@export var drag_coefficient: float
-@onready var drag:float  = 1.0 - drag_coefficient
 
 
 func _physics_process(_delta):
