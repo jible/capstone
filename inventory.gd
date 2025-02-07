c13d570176705f56c34dab82ee5f3aed0561a4a6
Jackson McLane <jmclane@ucsc.edu>
2025-02-07 00:11:00 -0800

    added currency costs to upgrades
    - upgrades now cost currency
    TODO
    - link to UI
    - convert upgrade scaling to use array

--- inventory.gd
+++ inventory.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+class_name Inventory
+
 var items = {}
 
 
@@ -7,7 +9,7 @@ func obtain_item(currency_type,amount):
 	items[currency_type] =  items.get(currency_type, 0 ) + amount
 	
 func check_item(type):
-	if items[type]:
+	if type in items:
 		return items[type]
 	return 0
 
