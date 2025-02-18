009975ba68331d4c3b16e85e5ce65c6baaf84951
Jack Sims <123803257+sym-z@users.noreply.github.com>
2025-02-18 12:56:59 -0800

    Added Death and Attack sounds for the Player
    Death sound will need additional code later to stop from being cut off, but it is fine for the playtest.

--- scripts/characters/player/states/death.gd
+++ scripts/characters/player/states/death.gd
@@ -4,6 +4,8 @@ extends SMState
 @export var character: CharacterBody2D
 @export var lock_direction = false
 @export var movement_details =  {}
+@export var speaker: AudioStreamPlayer2D
+var sfx = SoundManager
 var animation_name = "death"
 var direction_dependent = false
 var callbacks = {}
@@ -11,6 +13,7 @@ var callbacks = {}
 func update_state(delta):
 	pass
 func enter_state():
+	sfx.play_player_sound(speaker,sfx.PlayerSounds.DEATH)
 	character.hurtbox.turn_off()
 	SignalBus.player_die.emit()
 	Globals.change_scene(load(death_scene))
