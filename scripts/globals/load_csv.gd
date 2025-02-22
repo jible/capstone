229b7ff8dd4b6a5ba8e411874da437515b3e6f3a
Jackson McLane <jmclane@ucsc.edu>
2025-02-22 14:24:45 -0800

    upgrades now scale based off csv file

---
+++ scripts/globals/load_csv.gd
@@ -0,0 +1,25 @@
+#Based on Tutorial from Michael Games on YT
+#https://www.youtube.com/watch?v=EbkKFOB4FpI
+extends Node
+
+func load_csv_dictionary(csv_path: String) -> Dictionary:
+	var _csv_dictionary = {}
+	
+	var file = FileAccess.open(csv_path, FileAccess.READ)
+	
+	if file == null:
+		printerr("No file found at: ", csv_path)
+		return _csv_dictionary
+	
+	
+	#load rest of lines as arr values	
+	while not file.eof_reached():
+		var line = file.get_csv_line()
+		for value in line.size():
+			if value == 0:
+				_csv_dictionary[line[value]] = []
+			else:
+				_csv_dictionary[line[0]].append(int(line[value]))
+	file.close()
+	
+	return _csv_dictionary
--- scripts/globals/upgrade_manager.gd
+++ scripts/globals/upgrade_manager.gd
@@ -4,30 +4,27 @@ extends Node
 const HEALTH: String = "health"
 const DMG: String = "dmg"
 const SPEED: String = "speed"
-# hp and dmg are flat increases, starting at 0
-# speed is a mult increase, should start at 1
-# TODO
-# delink level from stat increase
-# start levels at 0,
-# convert to indices of an upgrades array (source of truth)
+const stat_sheet_path: String = "res://assets/csvs/Cast Into Flame Stat Sheet - upgrades.csv"
+
 var stat_lvl: Dictionary = {
 	"health": 0,
 	"dmg": 0,
-	"speed": 1
+	"speed": 0
 }
+
+var stat_growth: Dictionary = LoadCsv.load_csv_dictionary(stat_sheet_path)
+
 #amount to increase upgrades by
-var upgrade_growth: Dictionary = {
-	"health": 1,
-	"dmg": 1,
-	"speed": 1.2
+var upgrade_bonus: Dictionary = {
+	"health": 0,
+	"dmg": 0,
+	"speed": 0
 }
 var upgrade_cost = 1
 
 ##Death Scene
 var death_scene: PackedScene = preload(
 	"res://scenes/prefabs/ui_elements/death_screen.tscn")
-
-#Inventory ref from player
 
 func _ready(): 
 	SignalBus.upgrade_stat_button_pressed.connect(_on_stat_upgraded)
@@ -40,18 +37,18 @@ func check_can_upgrade(stat: String) -> bool:
 
 func upgrade_stat(stat: String):
 	stat_lvl[stat] += 1
+	upgrade_bonus[stat] += get_stat_growth(stat)
 	
-#TODO convert to use array indices from csv
 func get_stat(stat: String):
-	return stat_lvl[stat] * upgrade_growth[stat]
+	return upgrade_bonus[stat]
 
 func get_stat_lvl(stat: String):
 	return stat_lvl[stat]
 	
 func get_stat_growth(stat: String):
-	return upgrade_growth[stat]
+	return stat_growth[stat][stat_lvl[stat]]
 	
-#TODO convert to use array indices from csv
+#TODO convert to use array indices from csv?
 func get_upgrade_cost(stat: String):
 	if get_stat_lvl(stat) == 0:
 		return upgrade_cost
