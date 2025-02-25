0cf5d265ea7e1ecd6f7a07e5082e701dd7ebac2d
Jackson McLane <jmclane@ucsc.edu>
2025-02-25 14:18:43 -0800

    Controller UI navigation
    added controller input for inventory and accept UI
    added focus grabbing for ui navigation

--- scripts/for_scenes/option_menu.gd
+++ scripts/for_scenes/option_menu.gd
@@ -1,7 +1,11 @@
 extends Control
 
 @export var previous_menu: Control
+@onready var remap_button: Button = $"VBoxContainer/RemapButton"
 
 func _on_back_button_pressed() -> void:
 	self.hide()
 	previous_menu.show()
+
+func _on_draw() -> void:
+	remap_button.grab_focus()
