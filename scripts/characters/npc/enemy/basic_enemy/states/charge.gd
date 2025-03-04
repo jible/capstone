2aa4d766c370d01ff1fb994504ab030412ee2f40
Jack Sims <123803257+sym-z@users.noreply.github.com>
2025-03-04 13:37:56 -0800

    Hiding silhouette, enemies now tint red before charging

--- scripts/characters/npc/enemy/basic_enemy/states/charge.gd
+++ scripts/characters/npc/enemy/basic_enemy/states/charge.gd
@@ -5,9 +5,12 @@ extends SMState
 @export var lock_direction = false
 @export var movement_details =  {}
 @export var animation_name = "charge"
+@export var color : CanvasItem
 
 # On readys
 @onready var sm = get_parent()
+const CHARGE_COLOR : Color = Color(1,0.5,0.5,1)
+const DEFAULT_COLOR : Color = Color(1,1,1,1)
 var direction_dependent = false
 var callbacks = {
 	"end": Callable(self, "anim_done")
@@ -22,5 +25,7 @@ func update_state(delta):
 	pass
 func enter_state():
 	character.mobility_manager.input_direction = Vector2.ZERO
+	color.modulate = CHARGE_COLOR
 func exit_state():
+	color.modulate = DEFAULT_COLOR
 	pass
