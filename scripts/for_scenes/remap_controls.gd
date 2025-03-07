2cc21c10a9e3815e2c5d5a26aab0133b46071c67
Jackson McLane <jmclane@ucsc.edu>
2025-03-07 15:58:12 -0800

    created bind_control scene, removed unused code from remap_controls

--- scripts/for_scenes/remap_controls.gd
+++ scripts/for_scenes/remap_controls.gd
@@ -9,14 +9,6 @@ extends Control
 		
 @export var container: VBoxContainer
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 func create_control():
 	var control_nodes = get_tree().get_nodes_in_group("controls")
 	print(control_nodes)
