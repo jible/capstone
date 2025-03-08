567d7d7b9b175588d8b987322032a760a1482111
Jackson McLane <jmclane@ucsc.edu>
2025-03-08 13:21:00 -0800

    bind control node progress

--- scripts/for_scenes/bind_control.gd
+++ scripts/for_scenes/bind_control.gd
@@ -1,11 +1,21 @@
+#based off of Easy Input Settings Menu by DashNothing
+#https://www.youtube.com/watch?v=ZDPM45cHHlI
+
 extends Control
 
+@onready var binding_button: Button = $BindingButton
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var is_remapping: bool = false
 
+func _on_binding_button_pressed() -> void:
+	is_remapping = true
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _input(event: InputEvent) -> void:
+	if is_remapping:
+		if (
+			event is InputEventKey || 
+			(event is InputEventMouseButton && event.is_pressed())
+		):
+			#bind to input map
+			pass
+	
