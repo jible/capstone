0abcbaee720a228292324040724899e8b93ddac1
Jackson McLane <jmclane@ucsc.edu>
2025-03-11 01:32:47 -0700

    changes to remap menu & input map
    removed unused global input
    skeleton for separate kbm and joypad bindings

--- scripts/for_scenes/bind_control.gd
+++ scripts/for_scenes/bind_control.gd
@@ -1,43 +1,84 @@
 #based off of Easy Input Settings Menu by DashNothing
 #https://www.youtube.com/watch?v=ZDPM45cHHlI
+@tool
+
 extends Control
 class_name BindControl
 
-@export var control: String
+@export var control: String:
+	set(new_control):
+		control = new_control
+		update_binding_text(mk_binding_button)
+		update_binding_text(joypad_binding_button)
 
-@onready var binding_button: Button = %BindingButton
-@onready var control_label: Label = %ControlLabel
+@export var control_label: Label
+@export var mk_binding_button: Button
+@export var joypad_binding_button: Button
 
-var is_remapping: bool = false
+var mk_is_remapping: bool = false
+var joypad_is_remapping: bool = false
 
 func _ready() -> void:
-	update_texts(control, InputMap.action_get_events(control)[0])
+	update_binding_text(mk_binding_button)
+	update_binding_text(joypad_binding_button)
 
-func _on_binding_button_pressed() -> void:
-	is_remapping = true
-	binding_button.text = "Press button to bind"
+func _on_mk_rebind_button_pressed() -> void:
+	mk_is_remapping = true
+	mk_binding_button.text = "Press button to bind"
+	
+func _on_joypad_rebind_button_pressed() -> void:
+	joypad_is_remapping = true
+	joypad_binding_button.text = "Press button to bind"
 
 func _input(event: InputEvent) -> void:
-	if is_remapping:
-		if (
-			event is InputEventKey || 
-			(event is InputEventMouseButton && event.is_pressed()) ||
-			event is InputEventJoypadButton || 
-			event is InputEventJoypadMotion
-		):
-			#bind to input map
-			InputMap.action_erase_events(control)
-			InputMap.action_add_event(control, event)
-			update_texts(control, event)
-			is_remapping = false
-			
+	if mk_is_remapping:
+		if is_valid_mk_input(event):
+			remap_action(mk_binding_button, event)
 			accept_event()
+			mk_is_remapping = false
 			
-func set_control(control: String):
-	self.control = control
-	update_texts(control, InputMap.action_get_events(control)[0])
+	if joypad_is_remapping:
+		if is_valid_joypad_input(event):
+			remap_action(joypad_binding_button, event)
+			accept_event()
+			joypad_is_remapping = false
 
-func update_texts(control: String, event: InputEvent) -> void:
-	binding_button.text = event.as_text().trim_suffix(" (Physical)")
-	##TODO localize control using tr(control)
-	control_label.text = control.capitalize() + ": "
+func is_valid_mk_input(event: InputEvent) -> bool:
+	if ( 
+		event is InputEventKey || 
+		(event is InputEventMouseButton)
+		): 
+			return true
+	return false
+	
+func is_valid_joypad_input(event: InputEvent) -> bool:
+	if ( 
+		event is InputEventJoypadButton || 
+		(event is InputEventJoypadMotion && 
+		( event.axis == JOY_AXIS_TRIGGER_LEFT || event.axis == JOY_AXIS_TRIGGER_RIGHT ) ) 
+		): 
+			return true
+	return false
+	
+func remap_action(button: Button, event: InputEvent) -> void:
+	InputMap.action_erase_events(self.control)
+	InputMap.action_add_event(self.control, event)
+	update_binding_text(button, [event])
+
+func update_binding_text(
+		button: Button,
+		events: Array[InputEvent] = InputMap.action_get_events(control)
+	) -> void:
+		button.text = ""
+		for event in events:
+			button.text += event.as_text().trim_suffix(" (Physical)") + ", "
+		button.text = button.text.trim_suffix(", ")
+		##TODO localize control using tr(control)?
+		control_label.text = self.control.capitalize() + ": "
+
+func get_mk_events():
+	
+	pass
+	
+func get_joypad_events():
+	pass
--- scripts/for_scenes/remap_controls.gd
+++ scripts/for_scenes/remap_controls.gd
@@ -2,35 +2,34 @@
 
 extends Control
 
-@export var controls: Array[String]:
+var things = InputMap.get_actions()
+
+var controls: Array[StringName]:
 	set(new_control):
 		controls = new_control
 		if Engine.is_editor_hint():
-			create_control()
+			create_controls()
 		
 @export var container: VBoxContainer
 @export var bind_control: PackedScene
 
-@onready var grab_focus = $"VBoxContainer/BindControl/HBoxContainer/BindingButton"
+@onready var grab_focus = %BackButton
 
 func _ready() -> void:
+	if Engine.is_editor_hint():
+		InputMap.load_from_project_settings()
+		controls = InputMap.get_actions()
 	grab_focus.grab_focus()
-
-func create_control():
+	
+func create_controls():
 	for control in controls:
-		if control_exists(control):
-			if control_node_exists(control):
-				printerr("Input %s node already created (Duplicate input)" % control)
-			else:
-				create_control_node(control)
-		else:
-			printerr("Input name %s not found in InputMap, please bind it" % control)
+		if control_exists(control) && not control_node_exists(control):
+			create_control_node(control)
 
 func create_control_node(control: String) -> void:
 	var control_node: BindControl = bind_control.instantiate()
-	control_node.control = control
-	control_node.update_texts(control, InputMap.action_get_events(control)[0])
 	container.add_child(control_node)
+	control_node.control = control
 	control_node.owner = get_tree().edited_scene_root
 	control_node.name = control
 	control_node.add_to_group("controls")
@@ -39,12 +38,17 @@ func control_node_exists(control: String) -> bool:
 	var control_nodes = get_tree().get_nodes_in_group("controls")
 	for node in control_nodes:
 		if node.name == control:
+			printerr("Input %s node already created (Duplicate input)" % control)
 			return true
 	return false
 	
 func control_exists(control: String) -> bool:
-	return ProjectSettings.has_setting("input/%s" % control)
-
+	if ProjectSettings.has_setting("input/%s" % control):
+		if control.find("ui") != 0 && control.find("look") != 0 && control.find("debug") != 0:
+			return true
+	else:
+		printerr("Input name %s not found in InputMap, please bind it" % control)
+	return false
 
 func _on_back_button_pressed() -> void:
 	self.queue_free()
